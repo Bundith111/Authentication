655130a1ba168774955c4180871a1971dcf9eed5
MaoSreyPich <maosreypich771@gmail.com>
2025-05-28 10:21:58 +0700

    StaffR

--- src/components/auth/StaffR.vue
+++ src/components/auth/StaffR.vue
@@ -9,37 +9,37 @@
          <!-- creating classroom -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Creating Classroom</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="classname" placeholder="Class Name" class="mb-4 w-full text-xl" /><br><br>
+          <n-input v-model:value="classname" placeholder="Class Name" class="mb-4 w-full text-xl" /><br>
           <n-button type="primary" @click="submitClassroomReport" >Create</n-button>
           </div><br>
           <!-- creating event -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Creating Event</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="classname" placeholder="Event Name" class="mb-4 w-full  text-xl" /><br>
-          <n-input v-model:value="classname" placeholder="Date" class="mb-4 w-full text-xl" /><br>
-          <n-input v-model:value="classname" placeholder="Type of Event" class="mb-4 w-full  text-xl" /><br>
-          <n-input type="textarea" v-model:value="description" placeholder="Description" class="mb-4 w-full "/><br>
-          <n-select v-model:value="selectedUser" :options="users" placeholder="Sent To" class="mb-4 w-full"/>
+          <n-input v-model:value="eventname" placeholder="Event Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="date" placeholder="Date" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="typeofevent" placeholder="Type of Event" class="mb-4 w-full  text-xl" /><br>
+          <n-input type="textarea" v-model:value="descriptionevent" placeholder="Description" class="mb-4 w-full "/><br>
+          <n-select v-model:value="selectedUser" :options="eventsendtouser" placeholder="Sent To" class="mb-4 w-full"/>
           <n-button type="primary" @click="submitEventReport">Create</n-button>
           </div><br>
           <!-- creating division -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Creating Division</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="name" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="namedivision" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
           <n-input type="textarea" v-model:value="description" placeholder="Description" class="mb-4 w-full "/><br>
           <n-button type="primary" @click="submitDivisionReport">Create</n-button>
           </div><br>
           <!-- creating position -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Creating Position</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="name" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="nameposition" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
           <n-input type="textarea" v-model:value="description" placeholder="Description" class="mb-4 w-full "/><br>
           <n-button type="primary" @click="submitPositionReport">Create</n-button>
           </div><br>
           <!-- creating permission -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Creating Permission</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="name" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="namepermission" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
           <n-input type="textarea" v-model:value="description" placeholder="Description" class="mb-4 w-full "/><br>
           <n-select v-model:value="selectedDivision" :options="divisions" placeholder="Under Division" class="mb-4 w-full"/>
           <n-select v-model:value="selectedPosition" :options="positions" placeholder="Under Position" class="mb-4 w-full"/>
@@ -50,27 +50,30 @@
           <div class=" rounded shadow ">
           <n-input v-model:value="busname" placeholder="Bus Name" class="mb-4 w-full  text-xl" /><br>
           <n-input v-model:value="licenseplate" placeholder="License Plate" class="mb-4 w-full text-xl" /><br>
+          <n-select v-model:value="selectedDriver" :options="drivers" placeholder="Select Driver" class="mb-4 w-full"/>
           <n-input type="textarea" v-model:value="route" placeholder="Route" class="mb-4 w-full "/><br>
-          <n-select v-model:value="selectedDriver" :options="drivers" placeholder="Select Driver" class="mb-4 w-full"/><br>
           <n-button type="primary" @click="submitBusRegistrationReport">Create</n-button>
           </div><br>
           <!-- student assignment to bus-->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Student Assignment to Bus</h2>
           <div class=" rounded shadow ">
-          <n-select v-model:value="selectedStudent" :options="students" placeholder="Select Student" class="mb-4 w-full"/>
-          <n-select v-model:value="selectedBus" :options="buses" placeholder="Select Bus" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedStudent" :options="studentstobus" placeholder="Select Student" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedBus" :options="busnumber" placeholder="Select Bus" class="mb-4 w-full"/>
           <n-button type="primary" @click="submitStudentBusAssignmentReport">Submit</n-button>
           </div><br>
           <!-- student registration -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Student Registration</h2>
           <div class=" rounded shadow ">
-          <n-input v-model:value="name" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
-          <n-input v-model:value="gender" placeholder="Gender" class="mb-4 w-full  text-xl" /><br>
-          <n-input v-model:value="age" placeholder="Age" class="mb-4 w-full text-xl" /><br>
-          <n-input v-model:value="dob" placeholder="Date of Birth" class="mb-4 w-full text-xl" /><br>
-          <n-select v-model:value="selectedGuardian" :options="guardians" placeholder="Select Guardian" class="mb-4 w-full"/>
-          <n-select v-model:value="selectedPickupMethod" :options="pickupMethods" placeholder="Pickup Method" class="mb-4 w-full"/>
-          <n-select v-model:value="selectedStatus" :options="status" placeholder="Status" class="mb-4 w-full"/>
+          <n-input v-model:value="namestd" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedGenderstd" :options="genderstd" placeholder="Select Gender" class="mb-4 w-full"/>
+          <n-input v-model:value="agestd" placeholder="Age" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="dobstd" placeholder="Date of Birth" class="mb-4 w-full text-xl" /><br>
+          <n-select v-model:value="selectedGuardianstd" :options="guardianstd" placeholder="Select Guardian" class="mb-4 w-full"/>
+          <n-input v-model:value="admissiondatestd" placeholder="Admission Date" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="specialnotestd" placeholder="Special Note" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedClassstd" :options="classstd" placeholder="Select Class" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedPickupMethodstd" :options="pickupmethods" placeholder="Pickup Method" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedStatusstd" :options="statusstd" placeholder="Status" class="mb-4 w-full"/><br>
           <label for="documentUpload" class="block mb-2 text-sm font-medium ">Profile Picture:</label>
           <input type="file" id="documentUpload" @change="handleFileUpload" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" >
            <n-button type="primary" @click="submitStudentRegistrationReport">Create</n-button>
@@ -78,12 +81,12 @@
           <!-- issuing invoice -->
           <h2 class=" font-bold mb-4  h-[30px] text-center"> Issuing Invoice</h2>
           <div class=" rounded shadow ">
-          <n-select v-model:value="selectedStudent" :options="students" placeholder="Select Student" class="mb-4 w-full"/>
-          <n-select v-model:value="selectedInvoice" :options="invoices" placeholder="Invoice Type" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedinvoiceStudent" :options="invoicestudents" placeholder="Select Student" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedInvoiceType" :options="invoicetypes" placeholder="Invoice Type" class="mb-4 w-full"/>
           <n-input type="textarea" v-model:value="invoiceDescription" placeholder="Description" class="mb-4 w-full "/><br>
-          <n-input v-model:value="amount" placeholder="Amount" class="mb-4 w-full text-xl" /><br>
-          <n-input v-model:value="dueDate" placeholder="Due Date" class="mb-4 w-full text-xl" /><br>
-          <n-select v-model:value="selectedStatus" :options="status" placeholder="Status" class="mb-4 w-full"/><br>
+          <n-input v-model:value="invoiceamount" placeholder="Amount" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="invoicedueDate" placeholder="Due Date" class="mb-4 w-full text-xl" /><br>
+          <n-select v-model:value="invoiceselectedStatus" :options="invoicestatus" placeholder="Status" class="mb-4 w-full"/>
           <n-button type="primary" @click="submitInvoiceReport">Create</n-button>
           </div><br>
           <!-- Verifying Receipt -->
@@ -93,15 +96,77 @@
           <n-input v-model:value="invoiceid" placeholder="Invoice ID" class="mb-4 w-full " /><br>
           <n-input v-model:value="studentname" placeholder="Student Name" class="mb-4 w-full " /><br>
           <n-input v-model:value="paymentDate" placeholder="Payment Date" class="mb-4 w-full text-xl" /><br>
-          <n-input v-model:value="amount" placeholder="Amount Paid" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="amountpaid" placeholder="Amount Paid" class="mb-4 w-full text-xl" /><br>
           <n-input v-model:value="paymentmethod" placeholder="Payment Method" class="mb-4 w-full " /><br>
           <n-input v-model:value="transactionid" placeholder="Transaction ID" class="mb-4 w-full " /><br>
           <!-- 2 this get data from issuing invoice -->
           <!-- <n-input v-model:value="paidby" placeholder="Paid By" class="mb-4 w-full " /><br>
           <n-input v-model:value="issuedby" placeholder="Issued By" class="mb-4 w-full " /><br> -->
-          <n-select v-model:value="selectedStatus" :options="status" placeholder="Status" class="mb-4 w-full"/><br>
+          <n-select v-model:value="selectedStatusVerifying" :options="verifyingstatus" placeholder="Status" class="mb-4 w-full"/>
           <n-button type="primary" @click="submitVerifyingReceiptReport">Verrify</n-button>
           </div><br>
+           <!-- Register Authorised Person -->
+          <h2 class=" font-bold mb-4  h-[30px] text-center">Register Authorised Person </h2>
+          <div class=" rounded shadow ">
+          <n-input v-model:value="nameregisterauthorisedperson" placeholder="Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="phonenumberregisterauthorisedperson" placeholder="Phone Number" class="mb-4 w-full  text-xl" /><br>
+          <label for="documentUpload" class="block mb-2 text-sm font-medium ">Profile Picture:</label>
+          <input type="file" id="documentUpload" @change="handleFileUpload" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" >
+           <n-button type="primary" @click="submitRegisterAuthorisedPersonReport">Register</n-button>
+          </div><br>
+          <!-- Authorized Person Assignment to Student-->
+          <h2 class=" font-bold mb-4  h-[30px] text-center"> Authorized Person Assignment to Student</h2>
+          <div class=" rounded shadow ">
+          <n-select v-model:value="selectedStudent" :options="students" placeholder="Select Students" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedAuthorizedPerson" :options="authorisedperson" placeholder="Select Authorised Person" class="mb-4 w-full"/>
+          <n-input v-model:value="relationshiptoStudent" placeholder="Relationship to Student" class="mb-4 w-full  text-xl" /><br>
+          <n-button type="primary" @click="submitStudentBusAssignmentReport">Submit</n-button>
+          </div><br>
+           <!-- Teacher Registration-->
+          <h2 class=" font-bold mb-4  h-[30px] text-center">Teacher Registration </h2>
+          <div class=" rounded shadow ">
+          <n-select v-model:value="selectedTeacherRegistrationStaffID" :options="staffidteacher" placeholder="Existing Staffs" class="mb-4 w-full"/>
+          <n-input v-model:value="role" placeholder="Role in Class" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedTeacherRegistrationClassassignment" :options="classassignmentteacher" placeholder="Existing Classes" class="mb-4 w-full"/>
+          <label for="Qualifications" class="block mb-2 text-sm font-medium ">Qualifications:</label>
+          <input type="file" id="documentUpload" @change="handleFileUpload" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" >
+           <n-button type="primary" @click="submitTeacherRegisterReport">Register</n-button>
+          </div><br>
+            <!-- Staff Registration-->
+          <h2 class=" font-bold mb-4  h-[30px] text-center">Staff Registration </h2>
+          <div class=" rounded shadow ">
+          <n-select v-model:value="selectedStaffUserID" :options="userid" placeholder="UserID" class="mb-4 w-full"/>
+          <n-input v-model:value="stafffristname" placeholder="First Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="stafflastname" placeholder="Last Name" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="stafflage" placeholder="Age" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedStaffGender" :options="gender" placeholder="Gender" class="mb-4 w-full"/>
+          <n-input v-model:value="staffhiredate" placeholder="Hire Date" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedStaffPosition" :options="positions" placeholder="Position" class="mb-4 w-full"/>
+          <n-input v-model:value="staffphonenumber" placeholder="Phone Number" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="staffemail" placeholder="Email" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="staffaddress" placeholder="Address" class="mb-4 w-full  text-xl" /><br>
+          <n-input v-model:value="staffemploymentstatus" placeholder="Employment Status" class="mb-4 w-full  text-xl" /><br>
+          <n-select v-model:value="selectedStaffStatus" :options="status" placeholder="Status" class="mb-4 w-full"/>
+          <label for="Qualifications" class="block mb-2 text-sm font-medium ">Qualifications:</label>
+          <input type="file" id="documentUpload" @change="handleFileUpload" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" >
+          <n-button type="primary" @click="submitStaffRegisterReport">Create</n-button>
+          </div><br>
+          <!--Role creating -->
+          <h2 class=" font-bold mb-4  h-[30px] text-center">Role Creating</h2>
+          <div class=" rounded shadow ">
+          <n-input v-model:value="nameRolecreating" placeholder="Name" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="guardnamerolecreating" placeholder="Guard Name" class="mb-4 w-full text-xl" /><br>
+          <n-input v-model:value="tagrolecreating" placeholder=" Tag" class="mb-4 w-full text-xl" /><br>
+          <n-button type="primary" @click="submitRoleReport" >Create</n-button>
+          </div><br>
+          <!-- User Assignment to Role-->
+          <h2 class=" font-bold mb-4  h-[30px] text-center"> User Assignment to Role</h2>
+          <div class=" rounded shadow ">
+          <n-select v-model:value="selectedUserAssignmentToRole" :options="userassignmenttorole" placeholder="Existing User" class="mb-4 w-full"/>
+          <n-select v-model:value="selectedRoleAssignmentToRole" :options="roleusers" placeholder="Existing Role" class="mb-4 w-full"/>
+          <n-button type="primary" @click="submitUserRoleAssignment">Submit</n-button>
+          </div><br>
+
       </div>
     </div>
   </div>
@@ -117,7 +182,7 @@ import { ref } from 'vue'
 
 
 const selectedUser = ref(null)
-const users = [
+const eventsendtouser = [
   { label: 'Student', value: 'student' },
   { label: 'Guardian', value: 'guardian' },
   { label: 'Parent', value: 'parent' },
@@ -139,17 +204,96 @@ const drivers = [
   { label: 'Driver B', value: 'driverB' },
   { label: 'Driver C', value: 'driverC' },
 ]
-const students = [
+const studentstobus = [
   { label: 'Student A', value: 'studentA' },
   { label: 'Student B', value: 'studentB' },
   { label: 'Student C', value: 'studentC' },
 ]
-const buses = [
-  { label: 'Bus A', value: 'busA' },
-  { label: 'Bus B', value: 'busB' },
-  { label: 'Bus C', value: 'busC' },
+const busnumber = [
+  { label: 'Bus 1', value: 'bus1' },
+  { label: 'Bus 2', value: 'bus2' },
+  { label: 'Bus 3', value: 'bus3' },
 ]
-const className = ref('');
+const genderstd = [
+  { label: 'Male', value: 'male' },
+  { label: 'Female', value: 'female' },
+  { label: 'Other', value: 'other' },
+]
+const guardianstd = [
+  { label: 'Guardian A', value: 'guardianA' },
+  { label: 'Guardian B', value: 'guardianB' },
+  { label: 'Guardian C', value: 'guardianC' },
+]
+const classstd = [
+  { label: 'Class 1', value: 'class1' },
+  { label: 'Class 2', value: 'class2' },
+  { label: 'Class 3', value: 'class3' },
+]
+const statusstd = [
+  { label: 'Active', value: 'active' },
+  { label: 'Inactive', value: 'inactive' },
+]
+const invoicestudents = [
+  { label: 'Student A', value: 'studentA' },
+  { label: 'Student B', value: 'studentB' },
+  { label: 'Student C', value: 'studentC' },
+]
+const pickupmethods =[
+  { label: 'Bus', value: 'bus' },
+  { label: 'Walk', value: 'walk' },
+  { label: 'Car', value: 'car' },
+]
+const invoicetypes= [
+  { label: 'Tuition Fee', value: 'tuitionFee' },
+  { label: 'Transport Fee', value: 'transportFee' },
+  { label: 'Library Fee', value: 'libraryFee' },
+]
+const invoicestatus = [
+  { label: 'Paid', value: 'paid' },
+  { label: 'Unpaid', value: 'unpaid' },
+  { label: 'Pending', value: 'pending' },
+]
+const paymentmethod=[
+  { label: 'Cash', value: 'cash' },
+  { label: 'Credit Card', value: 'creditCard' },
+  { label: 'Bank Transfer', value: 'bankTransfer' },
+]
+const verifyingstatus = [
+  { label: 'Verified', value: 'verified' },
+  { label: 'Unverified', value: 'unverified' },
+]
+conststudents = [
+  { label: 'Student A', value: 'studentA' },
+  { label: 'Student B', value: 'studentB' },
+  { label: 'Student C', value: 'studentC' },
+]
+const authorisedperson = [
+  { label: 'Person A', value: 'personA' },
+  { label: 'Person B', value: 'personB' },
+  { label: 'Person C', value: 'personC' },
+]
+const staffidteacher = [
+  { label: 'Staff A', value: 'staffA' },
+  { label: 'Staff B', value: 'staffB' },
+  { label: 'Staff C', value: 'staffC' },
+]
+const classassignmentteacher = [
+  { label: 'Class A', value: 'classA' },
+  { label: 'Class B', value: 'classB' },
+  { label: 'Class C', value: 'classC' },
+]
+const userassignmenttorole = [
+  { label: 'User A', value: 'userA' },
+  { label: 'User B', value: 'userB' },
+  { label: 'User C', value: 'userC' },
+]
+const roleusers = [
+  { label: 'Role A', value: 'roleA' },
+  { label: 'Role B', value: 'roleB' },
+  { label: 'Role C', value: 'roleC' },
+]
+
+
 const handleFileUpload = (event) => {
   const file = event.target.files[0];
   if (!file) {
