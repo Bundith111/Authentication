a3e0a36cf4082b3478c44e86d0c4fad03f0dd098
vigona <vongvigonalove123@gmail.com>
2025-07-21 23:03:32 +0700

    guardianlist

--- src/store/modules/guardianstore.js
+++ src/store/modules/guardianstore.js
@@ -1,15 +1,15 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+// import { defineStore } from 'pinia'
+// import { ref } from 'vue'
 
-export const useGuardianStore = defineStore('guardian', () => {
-  const guardianList = ref([])
+// export const useGuardianStore = defineStore('guardian', () => {
+//   const guardianList = ref([])
 
-  const addGuardian = (guardian) => {
-    guardianList.value.push(guardian)
-  }
+//   const addGuardian = (guardian) => {
+//     guardianList.value.push(guardian)
+//   }
 
-  return {
-    guardianList,
-    addGuardian
-  }
-})
+//   return {
+//     guardianList,
+//     addGuardian
+//   }
+// })
